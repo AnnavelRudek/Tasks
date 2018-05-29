d520b5a5ce2386a3eba080d5c725765979cefff1
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-05-29 23:26:59 +0200

    14.3

---
+++ src/main/java/domain/TaskDto.java
@@ -0,0 +1,14 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+}
