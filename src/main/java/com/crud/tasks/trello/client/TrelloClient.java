f22628a1653be4561bfed0155738cfaa0a2b18f1
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-07-29 18:20:43 +0200

    18.2 a_feedback

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -4,18 +4,19 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TrelloClient {
 
-   // @Value("${trello.api.endpoint.prod}")
+   //@Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint="https://api.trello.com/1";
 
    //@Value("${trello.app.key}")
@@ -27,27 +28,38 @@ public class TrelloClient {
     //@Value("${trello.app.username}")
     private String trelloUsername="annapiwowarczyk1";
 
-
     @Autowired
     private RestTemplate restTemplate;
 
     public List<TrelloBoardDto> getTrelloBoards() {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/annapiwowarczyk1/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
-                .queryParam("username", trelloUsername)
-                .queryParam("fields", "name,id").build().encode().toUri();
+        @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        public List<TrelloBoardDto> getTrelloBoards () {
+            List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().stream().filter("TUTAJ FILTRACJA").collect(Collectors.toList());
+
+            return trelloBoards;
+
+        /*TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
         if (boardsResponse != null)
 
         {
             return Arrays.asList(boardsResponse);
         }
-        return new ArrayList<>();
+        return new ArrayList<>();*/
+        }
+    }
+
+    private URI getTrelloBoardsUrl() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/annapiwowarczyk1/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("username", trelloUsername)
+                .queryParam("fields", "name,id").build().encode().toUri();
     }
 
 }
+
+
 
