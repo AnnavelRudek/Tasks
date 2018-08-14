f8ea2b2dca538be3a7408637ac1e269db86120a0
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-08-14 13:32:11 +0200

    19.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -1,13 +1,19 @@
 package com.crud.tasks.scheduler;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.service.SimpleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
 
     @Autowired
     private SimpleEmailService simpleEmailService;
@@ -17,4 +23,17 @@ public class EmailScheduler {
 
     @Autowired
     private AdminConfig adminConfig;
+
+
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String message = "Currently in database you got: " + size + " task";
+        if (size != 1) {
+            message += "s";
+        }
+        simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, message, null));
+    }
+
 }
