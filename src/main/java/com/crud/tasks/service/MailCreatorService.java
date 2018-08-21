b65914642e4287f38b563538d1dc3ae4d260bbf4
AnnavelRudek <anna.tuszynska1@gmail.com>
2018-08-21 21:46:52 +0200

    24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,5 @@
 package com.crud.tasks.service;
 
-import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
